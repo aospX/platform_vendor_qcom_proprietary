952dcba01a55f81d7c9be979f92a3f48474d76d0
Rob Beane <robbeane@gmail.com>
2012-07-04 13:08:02 -0400

    Use BUILD_PREBUILT for CodeAurora libraries
    Change-Id: I8663f1f4eafb55aca13fd28e696bec598aa6366e
    Signed-off-by: Rob Beane <robbeane@gmail.com>

--- qcom-vendor.mk
+++ qcom-vendor.mk
@@ -12,8 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$(call inherit-product, vendor/qcom/proprietary/qcom-vendor-blobs.mk)
+# CodeAurora shared libraries
+PRODUCT_PACKAGES += \
+    libv8 \
+    libqc-opt \
+    libmm-color-convertor \
+    libmmosal \
+    libmmparser \
+    libmmparser_divxdrmlib \
+    libDivxDrm
 
 # Vendor extension library
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/system/lib/libqc-opt.so
+
+
+# All the blobs necessary for adreno
+PRODUCT_COPY_FILES += \
+    vendor/qcom/proprietary/system/etc/firmware/a225_pfp.fw:/system/etc/firmware/a225_pfp.fw \
+    vendor/qcom/proprietary/system/etc/firmware/a225_pm4.fw:/system/etc/firmware/a225_pm4.fw \
+    vendor/qcom/proprietary/system/etc/firmware/a225p5_pm4.fw:/system/etc/firmware/a225p5_pm4.fw \
+    vendor/qcom/proprietary/system/etc/firmware/a300_pfp.fw:/system/etc/firmware/a300_pfp.fw \
+    vendor/qcom/proprietary/system/etc/firmware/a300_pm4.fw:/system/etc/firmware/a300_pm4.fw \
+    vendor/qcom/proprietary/system/etc/firmware/leia_pfp_470.fw:/system/etc/firmware/leia_pfp_470.fw \
+    vendor/qcom/proprietary/system/etc/firmware/leia_pm4_470.fw:/system/etc/firmware/leia_pm4_470.fw \
+    vendor/qcom/proprietary/system/etc/firmware/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
+    vendor/qcom/proprietary/system/etc/firmware/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
+    vendor/qcom/proprietary/system/lib/egl/egl.cfg:/system/lib/egl/egl.cfg \
+    vendor/qcom/proprietary/system/lib/egl/eglsubAndroid.so:/system/lib/egl/eglsubAndroid.so \
+    vendor/qcom/proprietary/system/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
+    vendor/qcom/proprietary/system/lib/egl/libGLES_android.so:/system/lib/egl/libGLES_android.so \
+    vendor/qcom/proprietary/system/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
+    vendor/qcom/proprietary/system/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
+    vendor/qcom/proprietary/system/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so \
+    vendor/qcom/proprietary/system/lib/libC2D2.so:/system/lib/libC2D2.so \
+    vendor/qcom/proprietary/system/lib/libgsl.so:/system/lib/libgsl.so \
+    vendor/qcom/proprietary/system/lib/libOpenVG.so:/system/lib/libOpenVG.so \
+    vendor/qcom/proprietary/system/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so
